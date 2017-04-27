773d8eb37ebeed41c9bbc46ce60d6b874dabb51b
kvamme_NR <haavard.kvamme@gmail.com>
2017-04-27 11:53:29 +0200

    added karpathy agent

--- havakv_atari_multi.py
+++ havakv_atari_multi.py
@@ -70,14 +70,15 @@ class GameForContainer(GameAbstract):
 
 class MultiGame(GameAbstract):
     '''Used for playing multiple games simultaneously, on one core.'''
-    def __init__(self, gameName, agent, logfile=None):
+    def __init__(self, gameName, agent, logfile=None, render=False):
         super().__init__(gameName, agent, logfile)
         self.nbReplicates = self.agent.nbReplicates
+        self.render = render
 
     def setup(self):
         self.games = []
         for container in self.agent.containers:
-            self.games.append(GameForContainer(self.gameName, container, self.logfile))
+            self.games.append(GameForContainer(self.gameName, container, self.logfile, self.render))
         for game in self.games:
             game.setup()
 
@@ -221,7 +222,6 @@ class Agent(object):
         '''Draw an action for each container.'''
         preds = self.predict(self.currentStates())
         actions = self.policy(preds)
-        assert False, 'Need to make into list!!!!!'
         return actions
 
     def predict(self, states):
@@ -242,7 +242,10 @@ class StandardAtari_Container(Container):
         return self.states[-1]
 
     def _preprocessObservation(self, observation):
-        '''Make state from a new observation and append to states.'''
+        '''Make state from a new observation and append to states.
+
+        problem!!!!! We don't handle first new frame after done. It use the last state!!!!
+        '''
         observation = self.preprocessImage(observation)
         newState = np.zeros((1, self.agent.D, self.agent.D, self.agent.nbImgInState))
         if len(self.states) != 0:
@@ -368,7 +371,7 @@ class A2C(StandardAtari):
     decayRate = 0.99 # decay factor for RMSProp leaky sum of grad^2
     containerClass = A2C_Container
 
-    def __init__(self, nbReplicates, actionSpace, modelFileName, resume=False, setupModel=True, **kwargsContainer):
+    def __init__(self, nbReplicates, actionSpace, modelFileName, resume=False, **kwargsContainer):
         super().__init__(nbReplicates, **kwargsContainer)
         self.actionSpace = actionSpace
         self.nbActionClasses = len(actionSpace)
@@ -376,9 +379,7 @@ class A2C(StandardAtari):
         self.resume = resume
         self.nbUpdates = 0
 
-        if setupModel:
-            'How is setupModel different from resume?'
-            self.setupModel()
+        self.setupModel()
         self._makeActionClassMapping()
         self.episode = 0
         self.stepNumber = 0 # iterates every frame
@@ -406,10 +407,6 @@ class A2C(StandardAtari):
         self.valueModel is the prediction of the value function.
         self.model is the model with both outputs
         '''
-        if self.resume:
-            self.model = load_model(self.modelFileName)
-            # Need the other models as well...
-            return
         inputShape = (self.D, self.D, self.nbImgInState)
         model = self.deepMindAtariNet(self.nbActionClasses, inputShape, includeTop=False)
         inp = Input(shape=inputShape)
@@ -426,13 +423,15 @@ class A2C(StandardAtari):
         # Should we compile model?
 
         self.model = Model(inp, [action, value])
-        # loss = {'action': 'categorical_crossentropy', 'value': 'mse'}
-        # loss = {'action': categoricalCrossentropyWithWeights, 'value': 'mse'}
         actionAndEntropyLoss = makeActionAndEntropyLossA3C(self.entropyBeta)
         loss = {'action': actionAndEntropyLoss, 'value': 'mse'}
         loss_weights = {'action': 1, 'value': self.mseBeta}
         optim = RMSprop(self.learningRate, self.decayRate)
         self.model.compile(optim, loss) # Need to make it possible to set other optimizers
+
+        if self.resume:
+            self.model.load_weights(self.modelFileName)
+            return
 
     def _addValuePredsToAllContainers(self, valuePreds):
         '''Store valuePreds to all the containers.'''
@@ -473,10 +472,9 @@ class A2C(StandardAtari):
             container.reset()
 
         if self.nbUpdates % 30 == 0: 
-            self.model.save(self.modelFileName)
+            self.model.save_weights(self.modelFileName)
 
         self.nbUpdates += 1
-        # print(self.nbUpdates)
         
 
     def getDiscountedRewards(self):
@@ -573,3 +571,236 @@ def makeActionAndEntropyLossA3C(beta):
         entropy = entropyLoss(ypred)
         return policyLoss - beta * entropy # - because the entropy is positive with minimal values in 0 and 1
     return loss
+
+
+class A2C_withoutEntropy(A2C):
+    entropyBeta = 0
+
+
+
+class Karpathy_container(Container):
+
+    def setup(self):
+        '''Setup information in container.'''
+        super().setup()
+        self._lastStateBeforeUpdate = None
+        self.prev_x = None
+
+    def currentState(self):
+        if len(self.states) == 0:
+            return self._lastStateBeforeUpdate
+        return self.states[-1]
+
+    def _preprocessObservation(self, observation):
+        '''Proprocess observation. And store in states list'''
+        if self.prev_x is not None:
+            if self.isDone:
+                self.prev_x = None
+        cur_x = self.preprocessImage(observation)
+        x = cur_x - self.prev_x if self.prev_x is not None else np.zeros(self.agent.D)
+        self.prev_x = cur_x
+        x = x.reshape((1, -1))
+        self.states.append(x)
+
+    def preprocessImage(self, I):
+        '''Preprocess 210x160x3 uint8 frame into 6400 (80x80) 1D float vector'''
+        I = I[35:195] # crop
+        I = I[::2,::2,0] # downsample by factor of 2
+        I[I == 144] = 0 # erase background (background type 1)
+        I[I == 109] = 0 # erase background (background type 2)
+        I[I != 0] = 1 # everything else (paddles, ball) just set to 1
+        return I.astype(np.float).ravel()
+
+    def discountedRewards(self):
+        '''Return the discounted rewards.
+        Only works for pong as of now!!!!!!!!!!!!11
+        '''
+        """Take 1D float array of rewards and compute discounted reward """
+        r = np.vstack(self.rewards)
+        discounted_r = np.zeros_like(r)
+        running_add = 0
+        for t in reversed(range(0, r.size)):
+            if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
+            running_add = running_add * self.agent.gamma + r[t]
+            discounted_r[t] = running_add
+        return discounted_r
+
+    def reset(self):
+        '''Reset states. 
+        We save the last state as it is either the previous step, or
+        we are done and we have already loaded the first frame 
+        (see step functions).
+        '''
+        lastStateBeforeUpdate = self.currentState()
+        self.setup()
+        self._lastStateBeforeUpdate = lastStateBeforeUpdate
+
+
+
+class KarpathyPolicyPong(Agent):
+    '''Karpathy dense policy network.'''
+    H = 200 # number of hidden layer neurons
+    batch_size = 10 # every how many episodes to do a param update?
+    batchSize = 1024
+    learning_rate = 1e-3
+    gamma = 0.99 # discount factor for reward
+    decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
+    D = 80 * 80 # input dimensionality: 80x80 grid
+    containerClass = Karpathy_container
+
+    def __init__(self, nbReplicates, modelFileName, resume=False):
+        super().__init__(nbReplicates)
+        self.modelFileName = modelFileName
+        self.resume = resume
+        # self.prev_x = None
+        self.setupModel()
+
+    @property
+    def fullBatch(self):
+        '''True when we have enough data to make an update.'''
+        sizeOfData = sum([len(container.actions) for container in self.containers])
+        if sizeOfData < self.batchSize:
+            return False
+        return True
+
+    # def policy(self, pred):
+        # '''Returns an action based on given predictions.'''
+        # action = 2 if np.random.uniform() < pred else 3 # roll the dice!
+        # return action
+
+    def policy(self, pred):
+        actions = []
+        for p in pred:
+            action = 2 if np.random.uniform() < p[0] else 3 # roll the dice!
+            # sampleClass = np.random.choice(range(self.nbActionClasses), 1, p=p)[0]
+            # actions.append(self.class2Action[sampleClass])
+            actions.append(action)
+        return np.stack(actions)
+
+    # def update(self, reward, done, info):
+        # '''See update func in Agent class'''
+        # self.rewards.append(reward)
+        # if done:
+            # self.episode += 1
+            # self.prev_x = None
+            # if self.episode % self.batch_size == 0:
+                # self.updateModel()
+
+    def update(self):
+        '''Run to do a gradient update of our model.
+        We use data from all containers.
+        '''
+        self.updateModel()
+
+        for container in self.containers:
+            container.reset()
+
+        if self.nbUpdates % 30 == 0: 
+            self.model.save_weights(self.modelFileName)
+
+        self.nbUpdates += 1
+
+    def getStates(self, removeStatesWithoutActions=True):
+        '''Return all states.
+        removeStatesWithoutActions: Removes the current state if we have not taken
+            an action for it yet.
+        '''
+        states = []
+        for container in self.containers:
+            s = container.states
+            nbActions = len(container.actions)
+            nbStates = len(s)
+            if removeStatesWithoutActions and (nbActions < nbStates):
+                states.extend(s[:-(nbStates - nbActions)])
+            else:
+                states.extend(s)
+        return np.vstack(states)
+
+    def getActions(self):
+        '''Return all actions.'''
+        actions = []
+        for container in self.containers:
+            actions.extend(container.actions)
+        return np.vstack(actions)
+
+    def getDiscountedRewards(self):
+        '''Returns discounted rewards from containers.'''
+        dr = [container.discountedRewards() for container in self.containers]
+        return np.concatenate(dr)
+
+    def updateModel(self):
+        discountedRewards = self.getDiscountedRewards()
+        discountedRewards -= np.mean(discountedRewards)
+        discountedRewards /= np.std(discountedRewards)
+
+        X = self.getStates()
+        # fakeLabels = [self.action2Class[action] for action in self.getActions().flatten()]
+        fakeLabels = [1 if action == 2 else 0 for action in self.getActions().flatten()]
+        Y = np.vstack(fakeLabels)
+        # actionValues = discountedRewards - valuePreds
+        # Y = responseWithSampleWeights(Y, actionValues, self.nbActionClasses)
+        # self.model.train_on_batch(X, [Y, discountedRewards])
+        self.model.train_on_batch(X, Y, sample_weight=discountedRewards.reshape((-1,)))
+
+    # def updateModel(self):
+        # '''Should do all work with updating weights.'''
+        # print('Updating weights...')
+        # # stack together all inputs, actions, and rewards for this episode
+        # epx = np.vstack(self.states)
+        # fakeLabels = [1 if action == 2 else 0 for action in self.actions]
+        # epy = np.vstack(fakeLabels)
+        # epr = np.vstack(self.rewards)
+        # self.resetMemory()
+    
+        # # compute the discounted reward backwards through time
+        # discounted_epr = self._discountRewards(epr)
+        # # standardize the rewards to be unit normal (helps control the gradient estimator variance)
+        # discounted_epr -= np.mean(discounted_epr)
+        # discounted_epr /= np.std(discounted_epr)
+    
+        # # update our model weights (all in one batch)
+        # self.model.train_on_batch(epx, epy, sample_weight=discounted_epr.reshape((-1,)))
+
+        # if self.episode % (self.batch_size * 3) == 0: 
+            # self.model.save(self.modelFileName)
+
+    # def _discountRewards(self, r):
+        # """ take 1D float array of rewards and compute discounted reward """
+        # discounted_r = np.zeros_like(r)
+        # running_add = 0
+        # for t in reversed(range(0, r.size)):
+            # if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
+            # running_add = running_add * self.gamma + r[t]
+            # discounted_r[t] = running_add
+        # return discounted_r
+
+    def setupModel(self):
+        """Make keras model"""
+        if self.resume:
+            self.model = load_model(self.modelFileName)
+        else:
+            inp = Input(shape=(self.D,))
+            h = Dense(self.H, activation='relu')(inp)
+            out = Dense(1, activation='sigmoid')(h)
+            self.model = Model(inp, out)
+            optim = RMSprop(self.learning_rate, self.decay_rate)
+            self.model.compile(optim, 'binary_crossentropy')
+
+    # @staticmethod
+    # def _preprocess_image(I):
+        # '''Preprocess 210x160x3 uint8 frame into 6400 (80x80) 1D float vector'''
+        # I = I[35:195] # crop
+        # I = I[::2,::2,0] # downsample by factor of 2
+        # I[I == 144] = 0 # erase background (background type 1)
+        # I[I == 109] = 0 # erase background (background type 2)
+        # I[I != 0] = 1 # everything else (paddles, ball) just set to 1
+        # return I.astype(np.float).ravel()
+
+    # def preprocess(self, observation):
+        # '''Proprocess observation. And store in states list'''
+        # cur_x = self._preprocess_image(observation)
+        # x = cur_x - self.prev_x if self.prev_x is not None else np.zeros(self.D)
+        # self.prev_x = cur_x
+        # x = x.reshape((1, -1))
+        # self.states.append(x)
+
